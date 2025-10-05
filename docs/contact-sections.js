b32eed28fe09f9264145206048cc2ebf84a073e5
Mahmoud Dahy <seekpotato47@gmail.com>
2025-10-05 23:01:01 +0300

    Delete docs directory

--- docs/contact-sections.js
+++
@@ -1,7 +0,0 @@
-const sections = document.querySelectorAll(".contact-sections img");
-
-sections.forEach(section =>{
-    section.addEventListener('click', () =>{
-        document.getElementById(section.alt).scrollIntoView();
-    });
-});
--- docs/links-handling.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll("#links #links-buttons button");
-
-buttons.forEach(button =>{
-    button.addEventListener('click', () =>{
-        window.open(button.className, '_blank');
-    });
-});
